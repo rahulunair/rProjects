54dc6a103adaf424e7f4c68505bc9a5308fc5bad
rahulunair <rahulunair@gmail.com>
2015-03-23 00:19:46 -0500

    update logistic regression

--- regression_trees.R
+++ regression_trees.R
@@ -20,8 +20,18 @@ cor(Smarket[, -9])
 # Except for the significant relationship between volume and year we cannot find any
 # relationship between any other variables
 
+# splitting the dataset between train and test
+Smarket.train <- Smarket[Smarket$Year < 2005,] #998 observations
+Smarket.test <- Smarket[!(Smarket$Year < 2005), ] # 252 observations
+summary(Smarket.train)
+
 # modelling direction of market and other attributes using logistic regression.
-logit.model <-  glm (Direction ~ Volume + Lag1 + Lag2 + Lag3 + Lag4 + Lag5, family = binomial, data = Smarket)
+logit.model <-  glm (Direction ~ Volume + Lag1 + Lag2 + Lag3 + Lag4 + Lag5, family = binomial, data = Smarket.train)
+logit.predict <- predict(logit.model, type = 'response')
+logit.predict[1:10]
+length(logit.predict)
+logit.pred = rep("Down", 998)
+logit.pred[logit.predict > 0.5] = "Up"
 
-
+table(logit.pred, Smarket.train$Direction) # cross table of prediction and real direction
 
