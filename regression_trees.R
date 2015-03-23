fba1b1ac1eadd3c1206f66b45d2699e6f4835194
rahulunair <rahulunair@gmail.com>
2015-03-22 21:48:49 -0500

    Logistic regression and classifcation trees

---
+++ regression_trees.R
@@ -0,0 +1,10 @@
+# Logistic Regression
+
+#1.Using Smarket data (package "ISLR") build a logistic regression model
+#for predicting Direction (up and down) of stock price. 
+#In calculating the error you may use training data for (Year<2005) and 
+#test for (Year>=2005)
+#Find error rate for the training data and test data.
+
+########################################################################################
+
